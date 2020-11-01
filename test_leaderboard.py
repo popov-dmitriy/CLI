8ca86cb7f6f6d400571d39824fcea061e6f76f07
popov-dmitriy <71076068+popov-dmitriy@users.noreply.github.com>
2020-11-01 23:45:02 +0300

    Update test_leaderboard.py

--- test_leaderboard.py
+++ test_leaderboard.py
@@ -4,8 +4,8 @@ from leaderboard import get_stats
 
 def test_inverse_parameters():
     runner = CliRunner()
-    result1 = runner.invoke(get_stats, ["--mode", "r_wo", "--count", "2"])
-    result2 = runner.invoke(get_stats, ["--count", "2", "--mode", "r_wo"])
+    result1 = runner.invoke(get_stats, ["--mode", "r_wo", "--count", "15"])
+    result2 = runner.invoke(get_stats, ["--count", "15", "--mode", "r_wo"])
     assert result2.output == result1.output
     assert result1.exit_code == 0
     assert result2.exit_code == 0
@@ -14,7 +14,7 @@ def test_inverse_parameters():
 def test_country_count():
     runner = CliRunner()
     result = runner.invoke(get_stats, ["--mode", "r_wo", "--country", "ru"])
-    assert result.output == "3\n"
+    assert result.output == "25\n"
     assert result.exit_code == 0
 
 
@@ -22,7 +22,7 @@ def test_wrong_mode():
     runner = CliRunner()
     result = runner.invoke(get_stats, ["--mode", "qwer"])
     assert result.output == "This mode doesn't exist. Available mods: r_macguffin, r_wo, " \
-                            "r_rocket_arena_2, r_shaft_arena_1, r_ca_2, r_ca_1\n"
+                            "r_rocket_arena_2, r_shaft_arena_1, r_ca_2, r_ca_1.\n"
     assert result.exit_code == 0
 
 
