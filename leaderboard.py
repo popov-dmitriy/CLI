e9c924dc4abe7fe7215b4a90968c1e43769123eb
popov-dmitriy <71076068+popov-dmitriy@users.noreply.github.com>
2020-11-01 23:40:15 +0300

    Update leaderboard.py

--- leaderboard.py
+++ leaderboard.py
@@ -1,6 +1,8 @@
 import requests
 import click
 
+offset_size = 20
+
 
 @click.command()
 @click.option("--mode", "mode", required=True, help="game mode")
@@ -18,73 +20,77 @@ def get_stats(mode, user_id, count, country):
         return
 
     api_url = "https://www.diabotical.com/api/v0/stats/leaderboard"
-    params = {
-        'mode': mode,
-        'offset': 0
-    }
-    # make a request to API
-    try:
-        res = requests.get(api_url, params=params)
-    except requests.exceptions.ConnectionError:
-        result.append("Cannot connect to API")
-        print_info(result)
-        return
+    offset = 0
+    while True:
+        params = {
+            'mode': mode,
+            'offset': offset
+        }
+        offset += offset_size
+        # make a request to API
+        try:
+            res = requests.get(api_url, params=params)
+        except requests.exceptions.ConnectionError:
+            result.append("Cannot connect to API")
+            print_info(result)
+            return
+        # get data in json format and extract list from dictionary
+        data = res.json()['leaderboard']
+        # if we get empty response, it means that data ran out
+        if not data:
+            break
+        for player in data:
+            result.append(player)
+
+        if count is not None and offset >= count:
+            result = result[:count]
+            break
+
+        # in this case we only need to display only 20 players
+        if user_id is None and country is None and count is None:
+            break
 
     # if count is less than 0, print an error
     if count is not None and count < 0:
-        result.append("--count parameter cannot be less than 0")
-        print_info(result)
+        print_info("--count parameter cannot be less than 0")
         return
-
-    # get data in json format
-    data = res.json()
-    # extract list from dictionary
-    data = data['leaderboard']
 
     # There we need to select certain players
     if user_id:
-        for each_player in data:
-            if each_player['user_id'] == user_id:
-                result.append(each_player)
-        if not result:  # if there were not any player with specified id
-            result.append("It seems that there is no player with this id")
-            print_info(result)
+        certain_players = []
+        for player in result:
+            if player['user_id'] == user_id:
+                certain_players.append(player)
+        if not certain_players:  # if there were not any player with specified id
+            print_info("It seems that there is no player with this id")
+            return
         else:
-            if count is None:  # if count is specified -> need to display info about N players
-                print_info(result)
-            else:
-                print_info(result[:count])
+            print_info(certain_players)
 
     elif country:
         counter = 0
-        for each_player in data:  # count number of players in certain country
-            if each_player['country'] == country:
+        for player in result:  # count number of players in certain country
+            if player['country'] == country:
                 counter += 1
         if counter == 0:
-            result.append("There is no such country")
-            print_info(result)
+            print_info("There is no any player from such country")
             return
 
-        if count is None:  # if count wasn't specified, need to display number of players in certain country
-            result.append(counter)
-            print_info(result)
-        else:   # if count is specified, need to display minimum from 'count' and number of counted players
-            result.append(min([counter, count]))
-            print_info(result)
+        print_info(counter)
 
     else:
-        # delete field 'user_id'
-        for player in data:
+        for player in result:
+            # delete field 'user_id'
             del player['user_id']
-        if count is None:
-            print_info(data)
-        else:
-            print_info(data[:count])
+        print_info(result)
 
 
 def print_info(info):
-    for player in info:
-        print(player)
+    if isinstance(info, list):
+        for player in list(info):
+            print(player)
+    else:
+        print(info)
 
 
 if __name__ == '__main__':
